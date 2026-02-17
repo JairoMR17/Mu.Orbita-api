bb3b98f136a6922613f39f6dadec2e06fe79fcab
JairoMR17 <jairomejiasreyes@gmail.com>
2026-02-17 14:52:05 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,5 +7,6 @@ from app.models.parcel import Parcel
 from app.models.job import Job
 from app.models.kpi import Kpi
 from app.models.report import Report
+from .gee_image import GEEImage
 
 __all__ = ["Client", "Parcel", "Job", "Kpi", "Report"]
