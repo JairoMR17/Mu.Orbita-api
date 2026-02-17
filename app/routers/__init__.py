a903fc5dbb508be255d83b6c354da92209a8de4e
JairoMR17 <jairomejiasreyes@gmail.com>
2026-02-17 20:07:50 +0100

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -6,5 +6,6 @@ from app.routers.dashboard import router as dashboard_router
 from app.routers.webhooks import router as webhooks_router
 from app.routers.gee import router as gee_router
 from app.routers.reports import router as reports_router
+from .images import router as images_router
 
 __all__ = ["auth_router", "dashboard_router", "webhooks_router", "gee_router", "reports_router"]
